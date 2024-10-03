13cfa2705cb8e09a72a56356aee404c29f5e80c2
44Matt44 <matt123.3a@gmail.com>
2024-10-03 19:41:04 +0200

    Update CustomerRepository.cs

--- CustomerRepository.cs
+++ CustomerRepository.cs
@@ -1,20 +1,8 @@
-using System.Collections.Generic;
-using System.Linq;
-
-namespace BankIntegration.DataAccessLayer
+public class CustomerRepository : GenericRepository<Customer>
 {
-    public class CustomerRepository
+    public CustomerRepository(BankSystemContext context) : base(context)
     {
-        private readonly BankDbContext _context;
-
-        public CustomerRepository(BankDbContext context)
-        {
-            _context = context;
-        }
-
-        public List<Customer> GetCustomers()
-        {
-            return _context.Customers.ToList();
-        }
     }
+
+    // Add any custom customer-related methods here
 }
